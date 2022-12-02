c8fd4fcd5b12b161d3f52c5da861ade1c7d9f144
Kasper Birch <kasperbirch1@gmail.com>
2022-12-02 18:51:10 +0100

    add next-pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
+
 const sanityClient = require('@sanity/client')
 const client = sanityClient({
   dataset: process.env.SANITY_PROJECT_DATASET,
@@ -28,47 +32,49 @@ async function fetchSanityRedirects() {
   return redirects
 }
 
-module.exports = withBundleAnalyzer({
-  swcMinify: true,
-  env: {
-    // Needed for Sanity powered data
-    SANITY_PROJECT_DATASET: process.env.SANITY_PROJECT_DATASET,
-    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
-    SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
+module.exports = withPWA(
+  withBundleAnalyzer({
+    swcMinify: true,
+    env: {
+      // Needed for Sanity powered data
+      SANITY_PROJECT_DATASET: process.env.SANITY_PROJECT_DATASET,
+      SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+      SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
 
-    // Needed for Shopify product syncs
-    SHOPIFY_STORE_ID: process.env.SHOPIFY_STORE_ID,
-    SHOPIFY_STOREFRONT_API_TOKEN: process.env.SHOPIFY_STOREFRONT_API_TOKEN,
+      // Needed for Shopify product syncs
+      SHOPIFY_STORE_ID: process.env.SHOPIFY_STORE_ID,
+      SHOPIFY_STOREFRONT_API_TOKEN: process.env.SHOPIFY_STOREFRONT_API_TOKEN,
 
-    // Needed for Klaviyo forms
-    KLAVIYO_API_KEY: process.env.KLAVIYO_API_KEY,
+      // Needed for Klaviyo forms
+      KLAVIYO_API_KEY: process.env.KLAVIYO_API_KEY,
 
-    // Needed for Yotpo reviews
-    YOTPO_API_KEY: process.env.YOTPO_API_KEY,
-    YOTPO_SECRET_KEY: process.env.YOTPO_SECRET_KEY,
+      // Needed for Yotpo reviews
+      YOTPO_API_KEY: process.env.YOTPO_API_KEY,
+      YOTPO_SECRET_KEY: process.env.YOTPO_SECRET_KEY,
 
-    // Needed for Mailchimp forms
-    MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
-    MAILCHIMP_SERVER: process.env.MAILCHIMP_SERVER,
+      // Needed for Mailchimp forms
+      MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
+      MAILCHIMP_SERVER: process.env.MAILCHIMP_SERVER,
 
-    // Needed for SendGrid forms
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-  },
-  async redirects() {
-    const sanityRedirects = await fetchSanityRedirects()
-    return sanityRedirects
-  },
-  async headers() {
-    return [
-      {
-        source: '/robots.txt',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "connect-src 'self';",
-          },
-        ],
-      },
-    ]
-  },
-})
+      // Needed for SendGrid forms
+      SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    },
+    async redirects() {
+      const sanityRedirects = await fetchSanityRedirects()
+      return sanityRedirects
+    },
+    async headers() {
+      return [
+        {
+          source: '/robots.txt',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "connect-src 'self';",
+            },
+          ],
+        },
+      ]
+    },
+  })
+)
